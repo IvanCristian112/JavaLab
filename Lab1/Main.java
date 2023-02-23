4f84c1d159c0a71c2338ac0b078b418337fd4f67
IvanCristian <66778851+IvanCristian112@users.noreply.github.com>
2023-02-23 13:40:23 +0200

    Update Main.java

--- Lab1/Main.java
+++ Lab1/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        Compulsory();
+        //Compulsory();
+        //Homework
     }
 
     public static void Compulsory() {
@@ -21,5 +22,44 @@ public class Main {
         }
         System.out.println("Willy-nilly, this semester I will learn " + languages[result]);
     }
+     public static void Homework(String input) {
+        int dimension;
+        try {
+            dimension = Integer.parseInt(input);
+            System.out.println("Input valid");
+        } catch (NumberFormatException e) {
+            System.out.println("Input invalid");
+            return;
+        }
+        long startTime = System.nanoTime();
+        int[][] matrix = new int[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if (i == j) matrix[i][i] = 1;
+                else if (j > i) matrix[i][j] = j - i + 1;
+                else matrix[i][j] = j - i + dimension + 1;
+
+            }
+
+        }
+        if (dimension < 30) {
+            for (int i = 0; i < dimension; i++) {
+                StringBuilder concat = new StringBuilder();
+                for (int j = 0; j < dimension; j++) {
+                    concat.append(matrix[i][j]);
+                }
+                System.out.println("Linia " + i + ": " + concat);
+            }
+            for (int j = 0; j < dimension; j++) {
+                StringBuilder concat = new StringBuilder();
+                for (int i = 0; i < dimension; i++) {
+                    concat.append(matrix[i][j]);
+                }
+                System.out.println("Coloana " + j + ": " + concat);
+            }
+        } else {
+            long stopTime = System.nanoTime();
+            System.out.println((stopTime - startTime));
+        }
 
 }
