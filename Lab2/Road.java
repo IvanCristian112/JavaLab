6cba030f8f051dbf4f36e534ff6f26e28a601fe0
IvanCristian <66778851+IvanCristian112@users.noreply.github.com>
2023-03-02 13:36:23 +0200

    Update Road.java

--- Lab2/Road.java
+++ Lab2/Road.java
@@ -1,20 +1,29 @@
 public class Road {
-    private float length;
+    private int length;
     private RoadType type;
     private int speedLimit;
+    private Location locatie1;
+    private Location locatie2;
 
     public Road() {
     }
 
-    public Road(float length) {
-        this.length = length;
+    public Road(int length) {
+        int x1 = this.locatie1.xCoordinate;
+        int x2 = this.locatie2.xCoordinate;
+        int y1 = this.locatie1.yCoordinate;
+        int y2 = this.locatie2.yCoordinate;
+        int euclidianDistance = (x1 - x2) ^ 2 + (y1 - y2) ^ 2;
+        if (length >= euclidianDistance) {
+            this.length = length;
+        }
     }
 
     public float getLength() {
         return length;
     }
 
-    public void setLength(float length) {
+    public void setLength(int length) {
         this.length = length;
     }
 
