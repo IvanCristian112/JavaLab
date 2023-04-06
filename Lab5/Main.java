6e5769e0273a8b28a0cde320e7f60955c569dce6
IvanCristian <66778851+IvanCristian112@users.noreply.github.com>
2023-04-06 11:53:04 +0300

    Add files via upload

--- Lab5/Main.java
+++ Lab5/Main.java
@@ -8,19 +8,19 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-/*        var book = new Document("LFAC-Curs6", "C:\\Users\\Victor\\Downloads\\munte.jpg");
+        var book = new Document("LFAC-Curs6", "C:\\Users\\Victor\\Downloads\\munte.jpg");
         book.addTag("Livius Ciocarlie");
         var article = new Document("New aspects on API implementation", "C:\\Users\\Victor\\Downloads\\test.txt");
         article.addTag("Andrache Mihai");
-        var book2 = new Document("LFAC", "C:\\Users");
+        var book2 = new Document("LFAC", "C:\\Users\\Victor\\Downloads\\lfac4.pdf");
         book2.addTag("Livius Ciocarlie");
         var catalog = new Catalog("Java");
         AddCommand add = new AddCommand(catalog);
         add.add(book);
         add.add(article);
         add.add(book2);
-        System.out.println(catalog);*/
-/*        ViewCommand view = new ViewCommand();
+        System.out.println(catalog);
+        ViewCommand view = new ViewCommand();
         try {
             view.view(book);
         } catch (IOException e) {
@@ -33,9 +33,9 @@ public class Main {
             throw new RuntimeException(e);
         } catch (Exception e) {
             throw new RuntimeException(e);
-        }*/
-/*        InfoCommand info = new InfoCommand(catalog);
-        try{
+        }
+        InfoCommand info = new InfoCommand(catalog);
+        try {
             info.info();
         } catch (TikaException e) {
             throw new RuntimeException(e);
@@ -43,21 +43,16 @@ public class Main {
             throw new RuntimeException(e);
         } catch (SAXException e) {
             throw new RuntimeException(e);
-        }*/
+        }
 
-/*        ListCommand list = new ListCommand(catalog);
-        list.list();*/
-/*        JGraphTAlgorithm algorithm = new JGraphTAlgorithm(catalog);
-        int number1 = algorithm.solve();
-        System.out.println(number1);
-        GreedyColoringAlgorithm algorithm2 = new GreedyColoringAlgorithm(catalog);
-        int number2 = algorithm2.solve();
-        System.out.println(number2);*/
+        ListCommand list = new ListCommand(catalog);
+
         TestAlgorithm testAlgorithm = new TestAlgorithm();
+        testAlgorithm.simpleTest();
 
-        //testAlgorithm.testGreedyAlgorithm(1000);
         for (int i = 0; i < 10; i++) {
-            testAlgorithm.testJGraphAlgorithm(100);
+            testAlgorithm.testGreedyAlgorithm(1000);
+            //testAlgorithm.testJGraphAlgorithm(1000);
         }
 
 
